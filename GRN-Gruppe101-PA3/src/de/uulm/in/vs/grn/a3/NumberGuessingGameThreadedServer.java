f4a227b090e2cccbe289227e5f52fae60f71d783
Marco5de <marco.deuscher@t-online.de>
2018-11-05 13:22:37 +0100

    EOF fixed

--- GRN-Gruppe101-PA3/src/de/uulm/in/vs/grn/a3/NumberGuessingGameThreadedServer.java
+++ GRN-Gruppe101-PA3/src/de/uulm/in/vs/grn/a3/NumberGuessingGameThreadedServer.java
@@ -7,40 +7,27 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 
-    public class NumberGuessingGameThreadedServer {
+public class NumberGuessingGameThreadedServer {
 
-        private static final int PORT = 5555;
-        private static final int MAX_THREADS  = 4;
-        private static  ServerSocket server = null;
-        private static Executor executor = Executors.newFixedThreadPool(MAX_THREADS);
+    private static final int PORT = 5555;
+    private static final int MAX_THREADS = 4;
+    private static ServerSocket server = null;
+    private static Executor executor = Executors.newFixedThreadPool(MAX_THREADS);
 
-        public static void main(String[]args) throws IOException {
-            try{
-                server = new ServerSocket(PORT);
-                System.out.println("Server started.");
-            }catch (IOException e){
-                e.printStackTrace();
-            }
-
-            for(int i = 0; i < 4; i++){
-                while(!server.isClosed()) {
-                    Socket soc = server.accept();
-                    System.out.println("Client connected to Server");
-                    Runnable workerThread = new NumberGuessingGameRequestHandler(soc);
-                    executor.execute(workerThread);
-                }
-            }
-            //threadFactory();
+    public static void main(String[] args) throws IOException {
+        try {
+            server = new ServerSocket(PORT);
+            System.out.println("Server started.");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        /*
-        private static void threadFactory() throws IOException {
-            while(!server.isClosed()){
-                Socket soc = server.accept();
-                System.out.println("Client connected to Server");
-                new Thread(new NumberGuessingGameRequestHandler(soc)).start();
-            }
-
+        while (!server.isClosed()) {
+            Socket soc = server.accept();
+            System.out.println("Client connected to Server");
+            Runnable workerThread = new NumberGuessingGameRequestHandler(soc);
+            executor.execute(workerThread);
         }
-       */
     }
+
+}
 
